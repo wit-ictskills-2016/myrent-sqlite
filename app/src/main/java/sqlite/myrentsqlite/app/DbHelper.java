077682a5662d867a9ff5cfc3b8df7ee3c48b7abc
John Fitzgerald <usplitu@hotmail>
2016-10-08 10:01:39 +0100

    step09 Residence model complete

--- app/src/main/java/sqlite/myrentsqlite/app/DbHelper.java
+++ app/src/main/java/sqlite/myrentsqlite/app/DbHelper.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,22 +24,30 @@ public class DbHelper extends SQLiteOpenHelper
 
   static final String PRIMARY_KEY = "id";
   static final String GEOLOCATION = "geolocation";
+  static final String DATE = "date";
+  static final String RENTED = "rented";
+  static final String TENANT = "tenant";
+  static final String ZOOM = "zoom";
+  static final String PHOTO = "photo";
 
   Context context;
 
-  public DbHelper(Context context)
-  {
+  public DbHelper(Context context) {
     super(context, DATABASE_NAME, null, DATABASE_VERSION);
     this.context = context;
   }
 
   @Override
-  public void onCreate(SQLiteDatabase db)
-  {
+  public void onCreate(SQLiteDatabase db) {
     String createTable =
         "CREATE TABLE tableResidences " +
             "(id text primary key, " +
-            "geolocation text)";
+            "geolocation text," +
+            "date text," +
+            "rented text," +
+            "tenant text," +
+            "zoom text," +
+            "photo text)";
 
     db.execSQL(createTable);
     Log.d(TAG, "DbHelper.onCreated: " + createTable);
@@ -47,12 +56,18 @@ public class DbHelper extends SQLiteOpenHelper
   /**
    * @param residence Reference to Residence object to be added to database
    */
-  public void addResidence(Residence residence)
-  {
+  public void addResidence(Residence residence) {
     SQLiteDatabase db = this.getWritableDatabase();
     ContentValues values = new ContentValues();
     values.put(PRIMARY_KEY, residence.id.toString());
     values.put(GEOLOCATION, residence.geolocation);
+
+    values.put(DATE, String.valueOf(residence.date.getTime()));
+    values.put(RENTED, residence.rented == true ? "yes" : "no");
+    values.put(TENANT, residence.tenant);
+    values.put(ZOOM, Double.toString(residence.zoom));
+    values.put(PHOTO, residence.photo);
+
     // Insert record
     db.insert(TABLE_RESIDENCES, null, values);
     db.close();
@@ -73,8 +88,14 @@ public class DbHelper extends SQLiteOpenHelper
         cursor.moveToFirst();
         residence.id = UUID.fromString(cursor.getString(columnIndex++));
         residence.geolocation = cursor.getString(columnIndex++);
+        residence.date = new Date(Long.parseLong(cursor.getString(columnIndex++)));
+        residence.rented = cursor.getString(columnIndex++) == "yes" ? true : false;
+        residence.tenant = cursor.getString(columnIndex++);
+        residence.zoom = Double.parseDouble(cursor.getString(columnIndex++));
+        residence.photo = cursor.getString(columnIndex++);
       }
-    } finally {
+    }
+    finally {
       cursor.close();
     }
     return residence;
@@ -84,7 +105,8 @@ public class DbHelper extends SQLiteOpenHelper
     SQLiteDatabase db = this.getWritableDatabase();
     try {
       db.delete("tableResidences", "id" + "=?", new String[]{residence.id.toString() + ""});
-    } catch (Exception e) {
+    }
+    catch (Exception e) {
       Log.d(TAG, "delete residence failure: " + e.getMessage());
     }
   }
@@ -105,7 +127,11 @@ public class DbHelper extends SQLiteOpenHelper
         Residence residence = new Residence();
         residence.id = UUID.fromString(cursor.getString(columnIndex++));
         residence.geolocation = cursor.getString(columnIndex++);
-
+        residence.date = new Date(Long.parseLong(cursor.getString(columnIndex++)));
+        residence.rented = cursor.getString(columnIndex++) == "yes" ? true : false;
+        residence.tenant = cursor.getString(columnIndex++);
+        residence.zoom = Double.parseDouble(cursor.getString(columnIndex++));
+        residence.photo = cursor.getString(columnIndex++);
         columnIndex = 0;
 
         residences.add(residence);
@@ -140,8 +166,7 @@ public class DbHelper extends SQLiteOpenHelper
   }
 
   @Override
-  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
-  {
+  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
     db.execSQL("drop table if exists " + TABLE_RESIDENCES);
     Log.d(TAG, "onUpdated");
     onCreate(db);

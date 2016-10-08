30ad2a58d1e7129812517ef7e240d92cd51d6b52
John Fitzgerald <usplitu@hotmail>
2016-10-08 08:15:28 +0100

    step02

---
+++ app/src/main/java/sqlite/myrentsqlite/activities/MyRent.java
@@ -0,0 +1,16 @@
+package sqlite.myrentsqlite.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import sqlite.myrentsqlite.R;
+
+public class MyRent extends AppCompatActivity
+{
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_myrent);
+  }
+}
